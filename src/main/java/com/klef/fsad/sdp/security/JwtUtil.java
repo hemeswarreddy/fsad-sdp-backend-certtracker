624f3586f5389351d18fe768c85db5e12db5563b
hemeswarreddy <hemeswar82@gmail.com>
2026-04-09 21:51:00 +0530

    updated jwt code

--- src/main/java/com/klef/fsad/sdp/security/JwtUtil.java
+++ src/main/java/com/klef/fsad/sdp/security/JwtUtil.java
@@ -23,7 +23,7 @@ public class JwtUtil
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 2))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
